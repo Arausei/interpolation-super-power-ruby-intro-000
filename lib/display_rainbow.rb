4b519508099c198852f1e9eed16c605d62239840
Arausei Johnson <arausei@yahoo.com>
2021-03-24 15:45:57 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-def display_rainbow([R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: Violet]) 
+def display_rainbow([R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: Violet])
   puts "red, orange, yellow, green, blue, indigo, violet"
 end
